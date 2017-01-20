048e0b53d6d55b0d8586b459368a3b7b50e2c254
hs1900 <hs1900@nyu.edu>
2017-01-20 14:25:40 -0500

    Update test.m
    changes to include averaging the first 5 frames

--- test.m
+++ test.m
@@ -3,17 +3,16 @@ nFrames = vidObj.NumberOfFrames;
 vidHeight = vidObj.Height; 
 vidWidth = vidObj.Width;
 
-mov(1:nFrames)= struct('cdata', zeros(vidHeight, vidWidth, 3, 'uint8'))
+mov(1:nFrames)= struct('cdata', zeros(vidHeight, vidWidth, 3, 'uint8'), 'colormap' ,[]);
 for i = 1 : nFrames
   mov(i).cdata = read(vidObj, i); 
 end
 
 % Assuming first couple of frames can be used as background
-% Assuming fixed camera and stable background
-
-frameBG = 5;
+% Assuming fixed camera and stable backgrouend
+% Say we take the first 5 frames to create th background
+BG = mean(mov(1:5);
 threshold = 64;
-BG = mov(frameBG);
 
 for i = frameBG+1 : nFrames
   diff = mov(i) - BG;
