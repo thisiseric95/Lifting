fd911b8f62a2f97f74cf946ecaeb49556c2e2264
hs1900 <hs1900@nyu.edu>
2017-01-20 14:00:31 -0500

    Update test.m
    Using a fixed background

--- test.m
+++ test.m
@@ -5,5 +5,21 @@ vidWidth = vidObj.Width;
 
 mov(1:nFrames)= struct('cdata', zeros(vidHeight, vidWidth, 3, 'uint8'))
 for i = 1 : nFrames
-  mov(i).cdata = read(vidObj, k); 
+  mov(i).cdata = read(vidObj, i); 
 end
+
+% Assuming first couple of frames can be used as background
+% Assuming fixed camera and stable background
+
+frameBG = 5;
+threshold = 64;
+BG = mov(frameBG);
+
+for i = frameBG+1 : nFrames
+  diff = mov(i) - BG;
+  if diff >= threshold
+    th = 1;
+  else th = 0;
+  end
+end
+  
